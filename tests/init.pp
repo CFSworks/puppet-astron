216a0800c5f1afb84f1f188b593359a75b5df097
Sam Edwards <CFSworks@gmail.com>
2015-03-09 16:35:13 -0700

    Add "puppet apply" test Puppet file.

--- tests/init.pp
+++ tests/init.pp
@@ -10,3 +10,28 @@
 # http://docs.puppetlabs.com/guides/tests_smoke.html
 #
 include astron
+
+astron::daemon { 'primary':
+  dc_files => ['one', 'two', 'three'],
+
+  bind     => "localhost",
+  connect  => "remotehost",
+  threaded => false,
+}
+
+astron::uberdog { "LoginManager":
+  daemon => "primary",
+
+  id     => 5511,
+}
+
+astron::uberdog { "LoginManagerTwo":
+  daemon => "primary",
+
+  id     => 5512,
+}
+
+astron::role::stateserver { "MySS":
+  daemon  => primary,
+  control => 402000,
+}
